514ec6e8b08ad33ed8d4d04d1d245b78eadefc93
Akhilesh Nayak <nayakhilesh@gmail.com>
2013-02-15 19:17:49 -0500

    got rid of AiType enum

--- ConnectFour/src/connectfour/controller/ai/MultiAi.java
+++ ConnectFour/src/connectfour/controller/ai/MultiAi.java
@@ -1,8 +1,5 @@
 package connectfour.controller.ai;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import connectfour.controller.ai.abstrakt.AbstractAi;
 import connectfour.controller.ai.sub.DetDepthLimitedAi;
 import connectfour.controller.ai.sub.DetTimeLimitedAi;
@@ -11,76 +8,67 @@ import connectfour.controller.ai.sub.RandTimeLimitedAi;
 import connectfour.model.GameBoard;
 
 public class MultiAi implements Ai {
-	
-	private enum AiType {
-		DET_TIME, DET_DEPTH, RAND_TIME, RAND_DEPTH
-	}
-	
+
 	private AbstractAi detDepthLimitedAi;
 	private AbstractAi detTimeLimitedAi;
 	private AbstractAi randDepthLimitedAi;
 	private AbstractAi randTimeLimitedAi;
-	
+
+	private AbstractAi resolvedAi;
+
 	private boolean deterministic;
 	private boolean timeLimited;
-	private Map<AiType, AbstractAi> aiTypeToInstanceMap = new HashMap<AiType, AbstractAi>();
-	
-	public MultiAi(GameBoard board, boolean INIT_DET_AI, boolean INIT_TIME_LIMITED,
-			long INIT_TIME_LIMIT, int INIT_DEPTH_LIMIT) {
+
+	public MultiAi(GameBoard board, boolean INIT_DET_AI,
+			boolean INIT_TIME_LIMITED, long INIT_TIME_LIMIT,
+			int INIT_DEPTH_LIMIT) {
 		this.detDepthLimitedAi = new DetDepthLimitedAi(board);
 		this.detTimeLimitedAi = new DetTimeLimitedAi(board);
 		this.randDepthLimitedAi = new RandDepthLimitedAi(board);
 		this.randTimeLimitedAi = new RandTimeLimitedAi(board);
-		aiTypeToInstanceMap.put(AiType.DET_DEPTH, detDepthLimitedAi);
-		aiTypeToInstanceMap.put(AiType.DET_TIME, detTimeLimitedAi);
-		aiTypeToInstanceMap.put(AiType.RAND_DEPTH, randDepthLimitedAi);
-		aiTypeToInstanceMap.put(AiType.RAND_TIME, randTimeLimitedAi);
 		this.setDeterministic(INIT_DET_AI);
 		this.setTimeLimited(INIT_TIME_LIMITED);
 		this.setTimeLimit(INIT_TIME_LIMIT);
 		this.setDepthLimit(INIT_DEPTH_LIMIT);
 	}
-	
+
 	public int move() {
-		return resolveAi().move();
+		resolvedAi = resolveAi();
+		return resolvedAi.move();
 	}
-	
+
 	private AbstractAi resolveAi() {
-		return aiTypeToInstanceMap.get(resolveAiType());
-	}
-	
-	private AiType resolveAiType() {
 		if (!deterministic && !timeLimited) {
-			return AiType.RAND_DEPTH;
+			return randDepthLimitedAi;
 		} else if (!deterministic && timeLimited) {
-			return AiType.RAND_TIME;
+			return randTimeLimitedAi;
 		} else if (deterministic && !timeLimited) {
-			return AiType.DET_DEPTH;
+			return detDepthLimitedAi;
 		} else {
-			return AiType.DET_TIME;
+			return detTimeLimitedAi;
 		}
 	}
-	
+
 	public void setDeterministic(boolean deterministic) {
 		this.deterministic = deterministic;
 	}
-	
+
 	public void setTimeLimited(boolean timeLimited) {
 		this.timeLimited = timeLimited;
 	}
-	
+
 	public void setDepthLimit(int depthLimit) {
 		this.detDepthLimitedAi.setLimit(depthLimit);
 		this.randDepthLimitedAi.setLimit(depthLimit);
 	}
-	
+
 	public void setTimeLimit(long timeLimit) {
 		this.detTimeLimitedAi.setLimit(timeLimit);
 		this.randTimeLimitedAi.setLimit(timeLimit);
 	}
-	
+
 	public void stop() {
-		resolveAi().stop();
+		resolvedAi.stop();
 	}
 
 }
